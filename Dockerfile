63d24b68a58c82a8077b1719c197a1246a3b4516
ziying.wzc <ziying.wzc@taobao.com>
2017-07-22 15:13:23 +0800

    add start script

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,16 @@
-FROM vladshub/python-virtualenv
+FROM python:3-alpine3.6
 MAINTAINER daxingplay <daxingplay@gmail.com>
+
+ADD start.sh /
+
+RUN apk add --no-cache bash git ansible supervisor && \
+    mkdir -p /srv/ansible_ui && \
+    git clone https://github.com/alaxli/ansible_ui.git /srv/ansible_ui && \
+    cd /srv/ansible-ui && \
+    pip install -r requirements.txt && \
+    pip install PIL --allow-external PIL --allow-unverified PIL && \
+    apk del git
+
+EXPOSE 8000
+
+ENTRYPOINT ['bash', '/start.sh']
---
+++ start.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+SETTINGS_LOCAL=/srv/ansible_ui/desktop/core/internal/settings_local.py
+
+if [ ! -z '$DB_NAME' ]; then
+    sed -i -e "s/'NAME': 'ansible',/'NAME': '$DB_USER',/g" $SETTINGS_LOCAL
+fi
+
+if [ ! -z '$DB_PASSWORD' ]; then
+    sed -i -e "s/'PASSWORD': '****',/'PASSWORD': '$DB_PASSWORD',/g" $SETTINGS_LOCAL
+fi
+
+if [ ! -z '$DB_USER' ]; then
+    sed -i -e "s/'USER': 'ansibleuser',/'USER': '$DB_USER',/g" $SETTINGS_LOCAL
+fi
+
+if [ ! -z '$DB_HOST' ]; then
+    sed -i -e "s/'HOST': 'localhost',/'HOST': '$DB_HOST',/g" $SETTINGS_LOCAL
+fi
+
+if [ ! -z '$DB_PORT' ]; then
+    sed -i -e "s/'PORT': '3306',/'PORT': '$DB_PORT',/g" $SETTINGS_LOCAL
+fi
+
+ANSIBLE_PLAYBOOK=`which ansible-playbook`
+
+if [ ! -z '$ANSIBLE_PLAYBOOK' ]; then
+    sed -i -e "s/ANSIBLE_PLAYBOOK = '/envansible_dir/bin/ansible-playbook'/ANSIBLE_PLAYBOOK = '$ANSIBLE_PLAYBOOK'/g" $SETTINGS_LOCAL
+fi
+
+sed -i \
+    -e "s@/yourvirtualenv/bin/python@python@g" \
+    -e "s@/ansible_ui_dir/@/srv/ansible_ui/@g" \
+    -e "s@user = ansible@;user = ansible@g" \
+    celery-conf/supervisord.conf
+
+cd /srv/ansible_ui/
+
+python manage.py schemamigration desktop.apps.account --init
+python manage.py schemamigration desktop.apps.ansible --init
+python manage.py syncdb
+python manage.py migrate ansible
+python manage.py migrate account
+python manage.py migrate kombu.transport.django
+python manage.py migrate djcelery
+python manage.py migrate guardian
+
+cp ansible-conf/ansible.cfg ~/.ansible.cfg
+
+# start
+
+python manage.py runserver 0.0.0.0:8000
